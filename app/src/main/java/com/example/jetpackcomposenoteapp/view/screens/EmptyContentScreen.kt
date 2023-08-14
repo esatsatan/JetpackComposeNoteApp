501991edb88a7bfa76c07dbecdabfd28dc449d77
Esat Satan <esatsatan91@gmail.com>
2023-08-14 15:13:36 +0300

    EmptContent Screen added.

---
+++ app/src/main/java/com/example/jetpackcomposenoteapp/view/screens/EmptyContentScreen.kt
@@ -0,0 +1,51 @@
+package com.example.jetpackcomposenoteapp.view.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposenoteapp.R
+
+@Composable
+fun EmptyContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Icon(
+            modifier = Modifier.size(120.dp),
+            painter = painterResource(id = R.drawable.ic_sad_face),
+            contentDescription = "Sad Face Icon"
+        )
+        Text(
+            text = "Not Bulunamadı",
+            color = Color.LightGray,
+            fontWeight = FontWeight.Bold,
+            style = TextStyle.Default
+        )
+
+    }
+}
+
+@Composable
+@Preview
+fun preview() {
+    EmptyContent()
+}
